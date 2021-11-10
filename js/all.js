7c42d4548c61b205cb2ba4c3ec130b4151741c95
Allen <asd9fgh9@gmail.com>
2021-11-11 01:06:17 +0800

    update all

--- js/all.js
+++ js/all.js
@@ -45,9 +45,71 @@ var vm = new Vue({
             this.Oscore = 0;
             this.end = false;
         },
+        again(){
+            this.blocks = Array.from({ length: 9 }, function(d,i){
+                return {
+                    id: i+1,
+                    type: 0
+                }
+            });
+            this.state = this.previous_game;
+            this.step = 0;
+            this.end = false;
+        },
         go(block){
-            block.type = this.state;
-            this.state = -this.state;
+            for(var i = 0; i < 8; i++) {
+                var [a,b,c] = this.check_list[i];
+                var sum1 = this.blocks[a].type + this.blocks[b].type + this.blocks[c].type;
+                if(sum1 == 3 || sum1 == -3) {
+                    this.end = true;
+                } 
+            }
+            if(this.end == false) {
+                if(block.type == 0) {
+                    block.type = this.state;
+                    this.state = -this.state;
+                }
+                if(this.step <= 8) {
+                    this.step++;
+                }
+                if(this.step == 1) {
+                    this.previous_game = -this.previous_game;
+                }
+            }
+        }
+    },
+    computed: {
+        win_text() {
+            if(this.blocks.length != 0) {    
+                // 輸贏計算
+                for(var i = 0; i < 8; i++) {
+                    var [a,b,c] = this.check_list[i];
+                    var sum = this.blocks[a].type + this.blocks[b].type + this.blocks[c].type;
+                    if(sum == 3) {
+                        this.Oscore++;
+                        return "O 贏了!";
+                    } else if(sum == -3) {
+                        this.Xscore++;
+                        return "X 贏了!";
+                    }   
+                }
+                // 平手判斷
+                var type_all = [];
+                for(var s = 0; s < 9; s++) {
+                    type_all.push(this.blocks[s].type);
+                }
+                if(type_all.indexOf(0) == -1) {
+                    return "雙方平手!";
+                }
+            }
+            
+            return this.state == 1?"輪到 O 了":"輪到 X 了";
+        },
+        o_text() {
+            return this.Oscore;
+        },
+        x_text() {
+            return this.Xscore;
         }
     }
 });
